1ba31c198138347d7abc028744d2e3de061d6bcc
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-12-24 14:08:36 -0200

    Project Created, Project clean
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <h3>Hello friend</h3>
+    </>
+  )
+}
