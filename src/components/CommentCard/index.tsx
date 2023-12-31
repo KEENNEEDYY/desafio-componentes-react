f8d8044eeff257fe206b575398f4a01bf186cba0
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-12-31 19:58:51 -0200

    Comment card
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/components/CommentCard/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function CommentCard() {
+	return (
+		<div className="ctop-catalog-comment-card">
+			<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+					ea corporis aliquid ab, consequatur rem inventore ullam iste tempore, voluptate deleniti nihil.
+				</p>
+		</div>
+	)
+}
