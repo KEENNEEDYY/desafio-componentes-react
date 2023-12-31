1d0217a527b5eea2556cd0a4cd3819074bf913b3
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-12-31 20:36:22 -0200

    Footer component
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Footer() {
+    return(
+       <footer>
+            <div className="ctop-footer-container ctop-container">
+                <p>@dscarrostop</p>
+                <p><i>Rua da Pipoca, 150</i></p>
+            </div>
+       </footer>
+    )
+}
