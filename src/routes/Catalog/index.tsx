a70f141f00d8b3aa8d8e0723d3cb0e14e4a2181f
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-12-31 19:58:51 -0200

    Catalog page finish
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,16 +1,17 @@
 import './styles.css';
 
 import CatalogCard from '../../components/CatalogCard';
+import CommentCard from '../../components/CommentCard';
 
 export default function Catalog() {
     return(
         <>
-            <main className="ctop-catalog-main">
+            <main className="ctop-catalog-main ctop-align-itens">
                 <section id="ctop-catalog-section" className="ctop-container">
                     <div className="ctop-catalog-title">
                         <h1>Venha nos visitar</h1>
                     </div>
-                    <div className="ctop-catalog-cards-container">
+                    <div className="ctop-align-itens">
                         <CatalogCard />
                         <CatalogCard />
                         <CatalogCard />
@@ -18,8 +19,16 @@ export default function Catalog() {
                     </div>
                 </section>
                 <section id="ctop-comments-section" className="ctop-container">
-                    <div>
-                        Comments Section
+                    <div className="ctop-catalog-title">
+                        <h1>O que estão dizendo</h1>
+                    </div>
+                    <div className="ctop-align-itens">
+                        <CommentCard />
+                        <CommentCard />
+                        <CommentCard />
+                        <CommentCard />
+                        <CommentCard />
+                        <CommentCard />
                     </div>
                 </section>
             </main>
